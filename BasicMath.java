6d4684b9275013d585abc4c4770cb8c3b6b7bb45
Grant Peverett <grantpeverett@gmail.com>
2025-04-08 14:36:07 -0500

    'Add' generic value extender and java.doc written

--- BasicMath.java
+++ BasicMath.java
@@ -1,3 +1,16 @@
+/**
+ * BasicMath interface for calculator operations
+ * @author Grant Peverett
+ * @see <a href="https://github.com/Grantyy1/Calculator_App">GitHub Repository</a>
+ */
 public interface BasicMath {
+    /**
+     * Adds value to the calculator.
+     * @param <N> Generic numeric type (N)
+     * @param value is the value to be added.
+     */
+    <N extends Number> void add(N value);
+
 }
-//
+
+
