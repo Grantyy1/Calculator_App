67396659950766d6bffb0ebc07ab6153aad294d7
Grant Peverett <grantpeverett@gmail.com>
2025-04-09 20:55:33 -0500

    Heading fro Advanced calc created. Taking a break.

--- AdvanedCalc.java
+++ AdvanedCalc.java
@@ -1,3 +1,8 @@
+/**
+ * AdvanceCalc class is used to do advanced arithmetic operations on the calculator.
+ * @author Grant Peverett
+ * @see <a href="https://github.com/Grantyy1/Calculator_App">GitHub Repository</a>
+ */
 
 
 public class AdvanedCalc {
