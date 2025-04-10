47bb2beb005833b68bc21948c1b5c54f7a63a91e
Grant Peverett <grantpeverett@gmail.com>
2025-04-10 13:01:49 -0500

    Final changes made to Testbench.Java. Neatening it up.

--- TestBench.java
+++ TestBench.java
@@ -1,46 +1,42 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+/**
+ * Where the code will run and can be tested.
+ * @author Grant Peverett
+ * @see <a href="https://github.com/Grantyy1/Calculator_App">GitHub Repository</a>
+ */
+
 public class TestBench {
     public static void main(String[] args) {
-        // Create an advanced calculator
+
         AdvanceCalc calc = new AdvanceCalc();
 
-        // Test basic operations
         calc.clear();
         calc.updateDisplay();
 
         calc.add(10.22);
         calc.subtract(2.22);
 
-        // Test memory operations
         calc.memoryAdd();
 
-        // Display current value using direct access (as shown in the output example)
         System.out.println(calc.currentValue);
 
-        // More operations
         calc.multiply(10.0);
         calc.memorySubtract();
 
         calc.divide(2.0);
 
-        // Test advanced operations
         calc.setPrecision(4);
         calc.updateDisplay();
 
         calc.pow(2.0);
         calc.sqrt();
 
-        // Use memory
         System.out.println("Using memory value");
         calc.currentValue = calc.getMemoryValue();
         calc.updateDisplay();
         calc.add(-72.0);
 
-        // Clear memory
         calc.memoryClear();
 
-        // Clear calculator
         calc.clear();
     }
-    }
+}
