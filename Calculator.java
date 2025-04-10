451de214f91db67b11811bbb38bd1703086f2e11
Grant Peverett <grantpeverett@gmail.com>
2025-04-09 19:24:14 -0500

    Stucture changed for Calculator adding java docs now.

--- Calculator.java
+++ Calculator.java
@@ -6,108 +6,123 @@
 
 public abstract class Calculator implements BasicMath {
 
-        protected double previousValue;
-        protected double inputValue;
-        protected double currentValue;
-        protected char operator;
+    /**
+     *
+     */
+    protected double previousValue;
 
-        /**
-         * Constructor for Calculator
-         */
-        public Calculator() {
-            this.previousValue = 0.0;
-            this.inputValue = 0.0;
-            this.currentValue = 0.0;
-            this.operator = ' ';
-            System.out.println("Calculator On");
-        }
+    /**
+     *
+     */
+    protected double inputValue;
 
-        /**
-         * Add a number to the current value
-         * @param <N> Generic numeric type
-         * @param value The number to add
-         */
-        @Override
-        public <N extends Number> void add(N value) {
-            inputValue = value.doubleValue();
-            System.out.println("+ " + inputValue);
-            System.out.println("=============");
-            currentValue += inputValue;
-            previousValue = currentValue - inputValue;
-            operator = '+';
-            updateDisplay();
-        }
+    /**
+     *
+     */
+    protected double currentValue;
 
-        /**
-         * Subtract a number from the current value
-         * @param <N> Generic numeric type
-         * @param value The number to subtract
-         */
-        @Override
-        public <N extends Number> void subtract(N value) {
-            inputValue = value.doubleValue();
-            System.out.println("- " + inputValue);
-            System.out.println("=============");
-            currentValue -= inputValue;
-            previousValue = currentValue + inputValue;
-            operator = '-';
-            updateDisplay();
-        }
+    /**
+     *
+     */
+    protected char operator;
 
-        /**
-         * Multiply the current value by a number
-         * @param <N> Generic numeric type
-         * @param value The number to multiply by
-         */
-        @Override
-        public <N extends Number> void multiply(N value) {
-            inputValue = value.doubleValue();
-            System.out.println("* " + inputValue);
-            System.out.println("=============");
-            previousValue = currentValue;
-            currentValue *= inputValue;
-            operator = '*';
-            updateDisplay();
-        }
-
-        /**
-         * Divide the current value by a number
-         * @param <N> Generic numeric type
-         * @param value The number to divide by
-         */
-        @Override
-        public <N extends Number> void divide(N value) {
-            inputValue = value.doubleValue();
-            if (inputValue == 0) {
-                System.out.println("Error: Division by zero");
-                return;
-            }
-            System.out.println("/ " + inputValue);
-            System.out.println("=============");
-            previousValue = currentValue;
-            currentValue /= inputValue;
-            operator = '/';
-            updateDisplay();
-        }
-
-        /**
-         * Clear the calculator, resetting all numeric fields to zero
-         */
-        public void clear() {
-            this.previousValue = 0.0;
-            this.inputValue = 0.0;
-            this.currentValue = 0.0;
-            this.operator = ' ';
-            System.out.println("Calculator Cleared");
-        }
-
-        /**
-         * Update the display with the current value
-         */
-        public void updateDisplay() {
-            System.out.printf("%.2f%n", this.currentValue);
-        }
+    /**
+     * Constructor for Calculator
+     */
+    public Calculator() {
+        this.previousValue = 0.0;
+        this.inputValue = 0.0;
+        this.currentValue = 0.0;
+        this.operator = ' ';
+        System.out.println("Calculator On");
     }
+
+    /**
+     * Add a number to the current value
+     * @param <N> Generic numeric type
+     * @param value The number to add
+     */
+    @Override
+    public <N extends Number> void add(N value) {
+        inputValue = value.doubleValue();
+        System.out.println("+ " + inputValue);
+        System.out.println("=============");
+        currentValue += inputValue;
+        previousValue = currentValue - inputValue;
+        operator = '+';
+        updateDisplay();
+    }
+
+    /**
+     * Subtract a number from the current value
+     * @param <N> Generic numeric type
+     * @param value The number to subtract
+     */
+    @Override
+    public <N extends Number> void subtract(N value) {
+        inputValue = value.doubleValue();
+        System.out.println("- " + inputValue);
+        System.out.println("=============");
+        currentValue -= inputValue;
+        previousValue = currentValue + inputValue;
+        operator = '-';
+        updateDisplay();
+    }
+
+    /**
+     * Multiply the current value by a number
+     * @param <N> Generic numeric type
+     * @param value The number to multiply by
+     */
+    @Override
+    public <N extends Number> void multiply(N value) {
+        inputValue = value.doubleValue();
+        System.out.println("* " + inputValue);
+        System.out.println("=============");
+        previousValue = currentValue;
+        currentValue *= inputValue;
+        operator = '*';
+        updateDisplay();
+        }
+
+    /**
+     * Divides the current value by a number
+     * @param <N> Generic numeric type
+     * @param value The number to divide by
+     */
+    @Override
+    public <N extends Number> void divide(N value) {
+        inputValue = value.doubleValue();
+        if (inputValue == 0) {
+            System.out.println("Error: Division by zero");
+            return;
+        }
+        System.out.println("/ " + inputValue);
+        System.out.println("=============");
+        previousValue = currentValue;
+        currentValue /= inputValue;
+        operator = '/';
+        updateDisplay();
+    }
+
+    /**
+     * Clear the calculator, resetting all numeric fields to zero
+     */
+    public void clear() {
+        this.previousValue = 0.0;
+        this.inputValue = 0.0;
+        this.currentValue = 0.0;
+        this.operator = ' ';
+        System.out.println("Calculator Cleared");
+    }
+
+    /**
+     * Update the display with the current value
+     */
+    public void updateDisplay() {
+        System.out.printf("%.2f%n", this.currentValue);
+    }
+}
 
 
 
